18f35a072e5b81f22c8fdccf6c92b4e7ceed6c82
Dmitry Khaperets <dmitry.khaperets@mgid.com>
2019-07-25 16:37:21 +0300

    [Columns] Created an option hiddenColumns - list of hidden columns on initialize state

--- compiled/methods/toggle-column.js
+++ compiled/methods/toggle-column.js
@@ -4,12 +4,19 @@ module.exports = function (column) {
     var _this = this;
 
     if (!this.userControlsColumns) {
-        this.userColumnsDisplay = JSON.parse(JSON.stringify(this.allColumns));
+        var columns = JSON.parse(JSON.stringify(this.allColumns));
+
+        if (this.opts.hiddenColumns.length) {
+            columns = columns.filter(function (columnName) {
+                return !this.opts.hiddenColumns.includes(columnName);
+            });
+        }
+
+        this.userColumnsDisplay = columns;
         this.userControlsColumns = true;
     }
 
     if (this.userColumnsDisplay.includes(column)) {
-
         // can't have no columns
         if (this.userColumnsDisplay.length === 1) return;
 
